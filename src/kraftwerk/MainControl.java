1aba2b6f59557fd82b733160090bc8a0e393a636
haChristopher <chris77772@gmail.com>
2015-01-10 15:47:54 +0100

    Works now

--- src/kraftwerk/MainControl.java
+++ src/kraftwerk/MainControl.java
@@ -1,87 +1,62 @@
 package kraftwerk;
-
+ 
 public class MainControl {
-	
-	private final static int REACTOR_CRITICALTEMP = 2878;
-	private static final int REACTOR_START_TEMP = 0; 
-    private static final int REACTOR_HEAT_COEFFICIENT = 42;
-    private static final int PUMP_SPEED = 1;
-    private static final int PUMP_COEFFICENT = 1;
-    private static final int WATER_START_TEMP = 10; 
-    private static final int RIVER_START_TEMP = 10;
-    public static final Object LOCK = new Object();
-	
-    
-  
-	
-
-    private int riverTemp = WATER_START_TEMP;
-    private int reactorTemp = REACTOR_START_TEMP;
-    		
-    public void start(){
-    	
-    Reactor reactor = new Reactor(REACTOR_START_TEMP,REACTOR_HEAT_COEFFICIENT,this);
-    River river = new River(WATER_START_TEMP,this);
-    CoolingCircuit A = new CoolingCircuit(WATER_START_TEMP);
-    RiverExchanger riExchange = new RiverExchanger(A ,river);
-    ReactorExchanger reExchange = new ReactorExchanger(A,reactor,this);
-    Thread pump = new Thread(new Pump(PUMP_SPEED,A,PUMP_COEFFICENT,riExchange,reExchange,this));
-    Thread reactorT = new Thread(reactor);
-    
-    pump.start();
-    reactorT.start();
-    
-   
-    
-    try{
-    	
-    
-    Thread.sleep(20000);
-    }catch (InterruptedException e) {
-    	 e.printStackTrace();
-    
-    }
-    
-		
-
-	       
-	         pump.interrupt();
-	         reactorT.interrupt();
-
-	       
-	         System.exit(0);
-
-	 }
-    
-    
-    
-    
-  
-    
-    
-    
-	
-	//Ausgabe der Temperaturen
-	public void printTemperature(){
-		
-		System.out.println("Temperatur Reaktor :" +reactorTemp+ ", Temperatur Rueckfluss in Rhein: " + riverTemp);
-	}
-
-
-
-
-	public void onRiverTempChange(int temp) {
-		this.riverTemp=temp;
-	}
-	
-	
-	
-	public void onReactorTempChange(int temp){
-		this.reactorTemp =temp;
-	}
-	
-   
+ 
+        private final static int REACTOR_CRITICALTEMP = 2878;
+        private static final int REACTOR_START_TEMP = 0;
+        private static final int REACTOR_HEAT_COEFFICIENT = 42;
+        private static final int PUMP_SPEED = 100;
+        private static final int PUMP_COEFFICENT = 1;
+        private static final int WATER_START_TEMP = 10;
+        private static final int RIVER_START_TEMP = 10;
+        public static final Object LOCK = new Object();
+ 
+        private int riverTemp = WATER_START_TEMP;
+        private int reactorTemp = REACTOR_START_TEMP;
+ 
+        public void start() {
+ 
+                Reactor reactor = new Reactor(REACTOR_START_TEMP,
+                                REACTOR_HEAT_COEFFICIENT, this);
+                River river = new River(WATER_START_TEMP, this);
+                CoolingCircuit A = new CoolingCircuit(WATER_START_TEMP);
+                RiverExchanger riExchange = new RiverExchanger(A, river);
+                ReactorExchanger reExchange = new ReactorExchanger(A, reactor, this);
+                Thread pump = new Thread(new Pump(PUMP_SPEED, A, PUMP_COEFFICENT,
+                                riExchange, reExchange, this));
+                Thread reactorT = new Thread(reactor);
+ 
+                pump.start();
+                reactorT.start();
+ 
+                try {
+ 
+                        Thread.sleep(20000);
+                } catch (InterruptedException e) {
+                        e.printStackTrace();
+ 
+                }
+ 
+                pump.interrupt();
+                reactorT.interrupt();
+ 
+                System.exit(0);
+ 
+        }
+ 
+        // Ausgabe der Temperaturen
+        public void printTemperature() {
+ 
+                System.out.println("Temperatur Reaktor :" + reactorTemp
+                                + ", Temperatur Rueckfluss in Rhein: " + riverTemp);
+        }
+ 
+        public void onRiverTempChange(int temp) {
+                this.riverTemp = temp;
+        }
+ 
+        public void onReactorTempChange(int temp) {
+                this.reactorTemp = temp;
+        }
+ 
 }
-
-
-
--- src/kraftwerk/Pump.java
+++ src/kraftwerk/Pump.java
@@ -1,68 +1,64 @@
 package kraftwerk;
-
-import sun.applet.Main;
-
+ 
+ 
 public class Pump implements Runnable {
-
-	// Pumpleistung in Liter pro Sekunde
-	private int performance = 0;
-	private CoolingCircuit a;
-	private int waterpumped = 0;
-	private int cooeficent = 0;
-	RiverExchanger riExchange;
-	ReactorExchanger reExchange;
-	MainControl mc;
-
-	public Pump(int performance, CoolingCircuit coolingcircuit, int cooeficent,RiverExchanger riExchange,ReactorExchanger reExchange,MainControl mc) {
-		this.performance = performance;
-		this.a = coolingcircuit;
-		this.cooeficent = cooeficent;
-		this.riExchange=riExchange;
-		this.reExchange=reExchange;
-		this.mc=mc;
-	}
-
-	@Override
-	public void run() {
-
-		while (!Thread.currentThread().isInterrupted()) {
-
-			//synchronized (MainControl.LOCK) {
-
-				//try {
-
-					for (int i = 0; i < performance; i++) {
-						waterpumped++;
-					}
-
-				//	MainControl.LOCK.wait(1000 / cooeficent);
-
-					if (waterpumped >= 100) {
-						a.rotate();
-						exchange();
-						waterpumped -= 100;
-					}
-				//} catch (InterruptedException e) {
-				//	Thread.currentThread().interrupt();
-			//	}
-
-			//}
-		}
-
-	}
-
-	public int getPerformance() {
-		return this.performance;
-	}
-
-	public void setPerformance(int performance) {
-		this.performance = performance;
-	}
-	
-	private void exchange(){
-		riExchange.exchange();
-		reExchange.exchange();
-		mc.printTemperature();
-	}
-
+ 
+        // Pumpleistung in Liter pro Sekunde
+        private int performance;
+        private CoolingCircuit a;
+        private int waterpumped = 0;
+        private int cooeficent;
+        RiverExchanger riExchange;
+        ReactorExchanger reExchange;
+        MainControl mc;
+ 
+        public Pump(int performance, CoolingCircuit coolingcircuit, int cooeficent,RiverExchanger riExchange,ReactorExchanger reExchange,MainControl mc) {
+                this.performance = performance;
+                this.a = coolingcircuit;
+                this.cooeficent = cooeficent;
+                this.riExchange=riExchange;
+                this.reExchange=reExchange;
+                this.mc=mc;
+        }
+ 
+        @Override
+        public void run() {
+ 
+                while (!Thread.currentThread().isInterrupted()) {
+ 
+                        synchronized (MainControl.LOCK) {
+ 
+                                try {
+                                       
+                                        waterpumped+=performance;
+ 
+                                        if (waterpumped >= 100) {
+                                                a.rotate();
+                                                exchange();
+                                                waterpumped -= 100;
+                                        }
+                                        MainControl.LOCK.wait((long)(1000 / cooeficent));
+                                } catch (InterruptedException e) {
+                                        Thread.currentThread().interrupt();
+                                }
+ 
+                        }
+                }
+ 
+        }
+ 
+        public int getPerformance() {
+                return this.performance;
+        }
+ 
+        public void setPerformance(int performance) {
+                this.performance = performance;
+        }
+       
+        private synchronized void exchange(){
+                riExchange.exchange();
+                reExchange.exchange();
+                mc.printTemperature();
+        }
+ 
 }
